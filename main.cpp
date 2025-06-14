47e393f0294b32ecd91e22f0769fb16868ec042a
david-petrovic <david.petrovic@akane.waseda.jp>
2025-06-14 13:19:34 +0200

    perf: Adds while loop and limit, that the game of life can only run at max 24 frames per second

--- main.cpp
+++ main.cpp
@@ -6,8 +6,22 @@ int main()
 {
     const int WINDOW_HEIGHT = 1000;
     const int WINDOW_WIDTH = 1000; 
-    
+    int FPS = 24;
+
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Game of Life");
+    SetTargetFPS(FPS);
+
+    // Simulation Loop
+    while(WindowShouldClose() == false)
+    {
+        // 1. Event Handling
+
+        // 2. Updating State
+
+        // 3. Draw State
+        BeginDrawing();
+        EndDrawing();
+    }
 
     CloseWindow();
 }

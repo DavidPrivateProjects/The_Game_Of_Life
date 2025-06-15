9cdc4d15892321c7b3025172fea2e56fc8a42c21
david-petrovic <david.petrovic@akane.waseda.jp>
2025-06-15 21:24:18 +0200

    refactor: Makes grid wrap around for neighbor count.

--- simulation.cpp
+++ simulation.cpp
@@ -1,4 +1,6 @@
 #include "simulation.hpp"
+#include <vector>
+#include <utility>
 
 void Simulation::Draw()
 {
@@ -8,4 +10,29 @@ void Simulation::Draw()
 void Simulation::SetCellValue(int row, int column, int value)
 {
     grid.SetValue(row, column, value);
+}
+
+int Simulation::CountLiveNeighbors(int row, int column)
+{
+    int liveNeighbors = 0;
+    std::vector<std::pair<int, int>> neighborOffsets = 
+    {
+        // All offsets from a cell to it's neighbors
+        {-1, 0},
+        {1, 0},
+        {0, -1},
+        {0, 1},
+        {-1, -1},
+        {-1, 1},
+        {1, -1},
+        {1, 1}
+    };
+
+    for (const auto& offset : neighborOffsets)
+    {
+        int neighborRow = (row + offset.first + grid.GetRows()) % grid.GetRows();
+        int neighborColumn = (column + offset.second + grid.GetColumns()) % grid.GetColumns();
+        liveNeighbors += grid.GetValue(neighborRow, neighborColumn);
+    }
+    return liveNeighbors;
 }

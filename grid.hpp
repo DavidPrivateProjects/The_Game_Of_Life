d45ebac3954f44d5dfff2aa69d710d0045f232dd
david-petrovic <david.petrovic@akane.waseda.jp>
2025-06-15 13:05:38 +0200

    feat: Creats a grid cpp file to save how the grid class will be called.

--- grid.hpp
+++ grid.hpp
@@ -7,6 +7,7 @@ class Grid
     public:
         Grid(int width, int height, int cellSize)
         : rows(height/cellSize), columns(width/cellSize), cellSize(cellSize), cells(rows, std::vector<int>(columns, 0)) {};
+        void Draw();
     private:
         int rows;
         int columns;

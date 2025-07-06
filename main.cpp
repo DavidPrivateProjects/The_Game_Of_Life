b19f580d3b20b42423bf37d4d7b0cff127bfd327
david-petrovic <david.petrovic@akane.waseda.jp>
2025-07-06 12:09:21 +0200

    design: Change design to blue and change dimensions

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 
 int main() 
 {
-    const int WINDOW_HEIGHT = 1000;
-    const int WINDOW_WIDTH = 1000; 
+    const int WINDOW_HEIGHT = 600;
+    const int WINDOW_WIDTH = 1200; 
     Color GREY = {20, 20, 20, 255};
     int FPS = 24;
-    const int CELL_SIZE = 25;
+    const int CELL_SIZE = 4;
 
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Game of Life");
     SetTargetFPS(FPS);
